532bb387db894733acdbadeb3a39244c620611be
David Suarez <dsuarezv@gmail.com>
2016-01-12 16:59:09 +0100

    Initial implementation of the distribution class

---
+++ MonteCarlo/Distribution.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MonteCarlo
+{
+    public class Distribution
+    {
+        private const int DefaultSize = 5000;
+
+        private Random mRandom = new Random();
+        private double[] mItems;
+
+
+        public double this[int index]
+        {
+            get { return mItems[index]; }
+            set { mItems[index] = value; }
+        }
+
+        public int Count
+        {
+            get { return mItems.Length; }
+        }
+
+
+        public Distribution(): this(DefaultSize)
+        {
+            
+        }
+
+        public Distribution(int initialSize)
+        {
+            mItems = new double[initialSize];
+        }
+
+
+        public double GetMean()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetVariance()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public static Distribution Apply(Distribution d1, Func<double, double> op)
+        {
+            var result = new Distribution(d1.Count);
+
+            for (int i = 0; i < d1.Count; ++i)
+            {
+                result[i] = op(d1.GetRandomItem());
+            }
+
+            return result;
+        }
+
+        public static Distribution Apply(Distribution d1, Distribution d2, Func<double, double, double> op)
+        {
+            var result = new Distribution(d1.Count);
+
+            for (int i = 0; i < d1.Count; ++i)
+            {
+                result[i] = op(d1.GetRandomItem(), d2.GetRandomItem());
+            }
+
+            return result;
+        }
+
+        public static Distribution InitGaussian(double mean, double variance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Distribution InitGaussian(double fromValue, double toValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Distribution InitUniform(double mean, double variance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Distribution InitConstant(double value)
+        {
+            var result = new Distribution(DefaultSize);
+
+            for (int i = 0; i < DefaultSize; ++i) result[i] = value;
+
+            return result;
+        }
+
+        private double GetRandomItem()
+        {
+            return this[mRandom.Next(Count)];
+        }
+    }
+}
+
---
+++ MonteCarloTests/DistributionTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using MonteCarlo;
+
+namespace MonteCarloTests
+{
+    [TestFixture()]
+    public class DistributionTests
+    {
+        [Test()]
+        public void DistBasic()
+        {
+            var d1 = Distribution.InitConstant(4);
+            var d2 = Distribution.InitConstant(6);
+
+            var res = Distribution.Apply(d1, d2, (x, y) => x * y);
+
+            for (int i = 0; i < d1.Count; ++i)
+                Assert.AreEqual(res[i], 24);
+        }
+    }
+}
+
