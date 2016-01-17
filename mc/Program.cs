8eb83e4353a42e2e3b9c194047b47e628d434e48
David Suarez <dsuarezv@gmail.com>
2016-01-17 19:37:59 +0100

    Added some more operation to sample model.

--- mc/Program.cs
+++ mc/Program.cs
@@ -11,14 +11,19 @@ namespace mc
         {
             var in1 = new ModelInput("in1") { Distribution = Distribution.InitGaussian(10, 0.31) };
             var in2 = new ModelInput("in2") { Distribution = Distribution.InitGaussian(3, 0.31) };
+            var in3 = new ModelInput("in3") { Distribution = Distribution.InitGaussian(5, 0.61) };
+            var in4 = new ModelInput("c") { Distribution = Distribution.InitConstant(10) };
             var op1 = new ModelFormula("op1") { Formula = "in1 * in2" };
+            var op2 = new ModelFormula("op2") { Formula = "op1 * in3 * c" };
 
             var model = new Model();
-            model.Add(in1, in2, op1);
+            model.Add(in1, in2, op1, in3, in4, op2);
             model.Solve();
 
-            Console.WriteLine("Result:");
-            PrintDistroChart(op1.Output.RawItems);
+
+            Console.WriteLine("Result: mean: {0} stddev: {1}", op2.Output.GetMean(), op2.Output.GetStdDeviation());
+            Console.WriteLine("Result histogram:");
+            PrintDistroChart(op2.Output.RawItems);
         }
 
 
